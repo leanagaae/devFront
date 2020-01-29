f2caa1e3c2b8236fc519dc9d8455b6fc5dbe11e6
leanagaae <60425612+leanagaae@users.noreply.github.com>
2020-01-29 12:21:03 +0100

    d

--- monjs.js
+++ monjs.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   //$('h1').hide('slow');
   //$('h1').hide(5000);
 
-  for ( var i = 0; i < 5; i++) {
+  for ( var i = 0; i < 10; i++) {
     $( '<div class="col-1 boite">' ).appendTo('#effets');
   }
 });
